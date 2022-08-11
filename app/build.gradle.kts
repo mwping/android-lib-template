0f72f73b51b63a7c0053d79e232801234a41c717
孟卫平 <mwping1324@163.com>
2022-08-11 20:00:47 +0800

    build: Update dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -31,10 +31,10 @@ android {
 }
 
 dependencies {
-    implementation("androidx.core:core-ktx:1.7.0")
-    implementation("androidx.appcompat:appcompat:1.4.1")
-    implementation("com.google.android.material:material:1.5.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.3")
+    implementation("androidx.core:core-ktx:1.8.0")
+    implementation("androidx.appcompat:appcompat:1.5.0")
+    implementation("com.google.android.material:material:1.6.1")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("com.mwping.android:templatelib:1.0.1")
     testImplementation("junit:junit:4.+")
     androidTestImplementation("androidx.test.ext:junit:1.1.3")
--- build.gradle.kts
+++ build.gradle.kts
@@ -14,8 +14,8 @@ buildscript {
         mavenLocal()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:7.1.1")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.31")
+        classpath("com.android.tools.build:gradle:7.2.2")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.21")
         classpath("com.mwping.android.plugin:publishlib:1.0.0")
 
         // NOTE: Do not place your application dependencies here; they belong
